eb421ffb3e89f380439b05f82e89e139c88b714f
amevans1988 <65561005+amevans1988@users.noreply.github.com>
2020-06-27 15:51:24 -0400

    0627-afternoonUpdate
    Names on the master contact list can no long

---
+++ 01_Core/week02/submissions/scrapCode.js
@@ -0,0 +1,19 @@
+// const addToFamily = (index) => {
+//     familyGroup = lastNames[index];
+//     if (familyNames.includes(familyGroup)){
+//         // Add to existing list
+//     } else {
+//         familyNames.push(familyGroup);
+//         newHead = document.createElement('TH');
+//         newFamilyName = document.createTextNode(`${familyGroup}`)
+//         newHead.appendChild(newFamilyName);
+//         console.log(newHead)
+//         // newFamilyName = document.createTextNode(`${familyGroup}`)
+//         // newFamilyHeader = `${newHead.appendChild(newFamilyName)}`;
+//         // console.log(newFamilyHeader)
+//         tableFamilyNames.innerHTML = `${newHead}`;
+//         // tableFamilyNames.innerHTML = 
+//     }
+// }
+
+// tableDelete.innerHTML += `<p class='buttonEntries'><span class='delete-button'><button id=${i} onClick=deleteName(${i})><img id='deleteIcon' src="images/delete.png" width="15" height="15"></button> <button id='fam${i}' onClick=addToFamily(${i})><img id='deleteIcon' src="images/family.png" width="15" height="15"></button></span></p>`;
